5e2c043b6990b10144aca826bb6ad361b85f800e
Rouan Wilsenach <Rouan.Wilsenach@tesglobal.com>
2016-11-07 07:15:31 +0200

    provide checksum

--- Formula/brewpack.rb
+++ Formula/brewpack.rb
@@ -2,7 +2,7 @@ class Brewpack < Formula
   desc "A simple way to install your favourite packages and apps on your mac"
   homepage "https://github.com/rouanw/brewpack"
   url "https://github.com/rouanw/brewpack/archive/v0.1.0.zip"
-  # sha256 ""
+  sha256 "3af47b1fa0c931203e5e0c62253d38080298fad56c849909f9503a0f1722497c"
   version "v0.1.0"
 
   def install

42bcbb8fdea71f70d09c951f3001b4e941bb5746
Rouan Wilsenach <Rouan.Wilsenach@tesglobal.com>
2016-11-09 06:03:25 +0200

    bump version

--- Formula/brewpack.rb
+++ Formula/brewpack.rb
@@ -1,9 +1,9 @@
 class Brewpack < Formula
   desc "A simple way to install your favourite packages and apps on your mac"
   homepage "https://github.com/rouanw/brewpack"
-  url "https://github.com/rouanw/brewpack/archive/v0.1.1.zip"
-  # sha256 "3af47b1fa0c931203e5e0c62253d38080298fad56c849909f9503a0f1722497c"
-  version "v0.1.1"
+  url "https://github.com/rouanw/brewpack/archive/v0.2.0.zip"
+  sha256 "7a3ff4ae7ce74b778c2b082657eb0e5face6cf3cbc81acd315b4f70be03c3a82"
+  version "v0.2.0"
 
   def install
     bin.install 'brewpack'
